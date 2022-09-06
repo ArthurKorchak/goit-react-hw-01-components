07494023edf96a2214fddd19740fed22d9915def
ArthurKorchak <korchak.arthur@gmail.com>
2022-09-06 15:36:03 +0300

    upd

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -6,7 +6,7 @@ const colors = { 22: 'red', 4: 'blue', 17: 'green', 47: 'gray', 10: 'brown' }
 function Statistics({title, stats}) {
     return (
         <section className={s.statistics}>
-            {title ? <h2 className={s.title}>{title}</h2> : null}
+            {title && <h2 className={s.title}>{title}</h2>}
             <ul className={s.statList}>
                 {stats.map(({id, label, percentage}) => (
                     <li className={s.item} key={id} style={{backgroundColor: colors[percentage],}}>
@@ -20,7 +20,7 @@ function Statistics({title, stats}) {
 };
 
 Statistics.propTypes = {
-    title: PropTypes.string.isRequired,
+    title: PropTypes.string,
     stats: PropTypes.arrayOf(
         PropTypes.shape({
             id: PropTypes.string.isRequired,
